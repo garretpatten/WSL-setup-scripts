96985d7dcebdbc9a0cbfa4f08b9547938fd96e84
Garret Patten <garret.patten@proton.me>
2024-08-12 19:11:44 -0400

    Add scripts for Kali setup

---
+++ src/scripts/master.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+workingDirectory=$(pwd)
+
+bash "$workingDirectory/src/scripts/pre-install.sh"
+
+# Home directory customization
+bash "$workingDirectory/src/scripts/organizeHome.sh"
+
+# CLI tools
+bash "$workingDirectory/src/scripts/cli.sh"
+
+# Productivity programs
+bash "$workingDirectory/src/scripts/productivity.sh" "$workingDirectory"
+
+# Security and privacy utilities
+bash "$workingDirectory/src/scripts/security.sh" "$workingDirectory"
+
+# IDE setup
+bash "$workingDirectory/src/scripts/ide.sh" "$workingDirectory"
+
+# Dev tools
+bash "$workingDirectory/src/scripts/dev.sh"
+
+# Penetration testing tools and wordlists
+bash "$workingDirectory/src/scripts/hacking.sh" "$workingDirectory"
+
+zsh "$workingDirectory/src/scripts/shell.sh" "$workingDirectory"
+
+bash "$workingDirectory/src/scripts/post-install.sh" "$workingDirectory"
